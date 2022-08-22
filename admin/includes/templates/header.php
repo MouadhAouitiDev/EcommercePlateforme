12fe21d0ef74df949cf0aa9d44d265bb1750f8eb
Mouadh Aouiti <a.mouadh@emerya.fr>
2022-08-22 02:32:34 +0200

    Project structure

---
+++ admin/includes/templates/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Admin</title>
+</head>
+Hello this is header
+<body>
---
+++ admin/includes/templates/index.php
@@ -0,0 +1,9 @@
+<?php include (__DIR__ . "../header.php");?>
+
+
+
+Hello this is content 
+
+
+
+<?php include (__DIR__ . "../footer.php");?>
